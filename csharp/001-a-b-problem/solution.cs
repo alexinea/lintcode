5fcc8675fe35d0c7cd8e9502a4c088cc9cf5ebe0
alexinea <forerunner@outlook.com>
2016-04-27 01:13:50 +0800

    bug fixed in lintcode-problem-001

--- csharp/001-a-b-problem/solution.cs
+++ csharp/001-a-b-problem/solution.cs
@@ -10,13 +10,25 @@ namespace LeetCode
             Console.WriteLine(Add(1, 1)); //=>2
             Console.WriteLine(Add(2, 3)); //=>5
             Console.WriteLine(Add(453, 777)); //=> 1230
-            Console.WriteLine(Add(-7,-7)); //=> -14
+            Console.WriteLine(Add(-7, -7)); //=> -14
+            Console.WriteLine(Add(-1, 1)); //=> 0
             Console.ReadLine();
         }
 
         static int Add(int x, int y)
         {
-            return ((x & y) << 1) + (x ^ y);
+            var a = x ^ y;
+            var b = x & y;
+
+            while (b != 0)
+            {
+                var m = a;
+                var n = b << 1;
+                a = m ^ n;
+                b = m & n;
+            }
+
+            return a;
         }
 
     }
