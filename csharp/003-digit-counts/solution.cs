010e8b65dc6f9ca33dc36c768525c8f2e15c3d9c
alexinea <forerunner@outlook.com>
2016-04-27 09:55:02 +0800

    lintcode-problem-003 by csharp

---
+++ csharp/003-digit-counts/solution.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LeetCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DigitCounts(1, 12)); //=>5
+            Console.ReadLine();
+        }
+
+        static int DigitCounts(int k, int n)
+        {
+            int counter = 0;
+            string sk = "" + k;
+
+            for (var i = 0; i <= n; i++)
+            {
+                string a = "" + i;
+                int f = a.Length - a.Replace(sk, "").Length;
+                counter += f;
+            }
+
+            return counter;
+        }
+    }
+}
