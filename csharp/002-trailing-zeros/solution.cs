c33ac89d77589e332b4de846eb3bd0d839fe72ca
alexinea <forerunner@outlook.com>
2016-04-27 09:30:57 +0800

    lintcode-problem-002 by csharp

---
+++ csharp/002-trailing-zeros/solution.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(TrailingZeros(13)); //=>2
+            Console.WriteLine(TrailingZeros(20)); //=>4
+            Console.WriteLine(TrailingZeros(105)); //=>25
+            Console.WriteLine(TrailingZeros(1001171717)); //=>250292920
+            Console.ReadLine();
+        }
+
+        static long TrailingZeros(long n)
+        {
+            long counter = 0;
+            long i = 1;
+
+            while (true)
+            {
+                i *= 5;
+                counter += n / i;
+                if (i > n)
+                    break;
+            }
+
+            return counter;
+        }
+    }
+}
